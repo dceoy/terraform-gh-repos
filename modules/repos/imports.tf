d45b2fb3a9595aa66d9ab7b1efb3c946b333e906
Daichi Narushima <1938249+dceoy@users.noreply.github.com>
2026-08-24 02:00:04 +0900

    Align ruleset imports with managed instances

--- modules/repos/imports.tf
+++ modules/repos/imports.tf
@@ -1,18 +1,12 @@
 import {
-  for_each = {
-    for name, repo in var.repositories : name => repo
-    if repo.import_existing
-  }
+  for_each = var.repositories
 
   to = github_repository.this[each.key]
   id = each.key
 }
 
 import {
-  for_each = {
-    for name, repo in local.active_repositories : name => repo
-    if repo.import_existing
-  }
+  for_each = local.active_repositories
 
   to = github_workflow_repository_permissions.this[each.key]
   id = each.key
@@ -20,6 +14,11 @@ import {
 
 # Adopt the existing ruleset while moving ruleset policy out of repository inventory.
 import {
-  to = github_repository_ruleset.default_branch["terraform-gh-repos"]
-  id = "terraform-gh-repos:20934253"
+  for_each = {
+    for name, repo in local.public_repositories : name => repo
+    if name == "terraform-gh-repos"
+  }
+
+  to = github_repository_ruleset.default_branch[each.key]
+  id = "${each.key}:20934253"
 }
