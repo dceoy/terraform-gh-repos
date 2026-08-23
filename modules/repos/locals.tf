aba8b65158aac37c876a8d29dfd942a515f99e89
Daichi Narushima <1938249+dceoy@users.noreply.github.com>
2026-08-24 01:59:49 +0900

    Simplify repository visibility handling

--- modules/repos/locals.tf
+++ modules/repos/locals.tf
@@ -4,18 +4,8 @@ locals {
     if !repo.archived
   }
 
-  existing_public_repositories = {
+  public_repositories = {
     for name, existing in data.github_repository.existing : name => var.repositories[name]
     if !var.repositories[name].archived && existing.visibility == "public"
   }
-
-  new_public_repositories = {
-    for name, repo in local.active_repositories : name => repo
-    if !repo.import_existing
-  }
-
-  public_repositories = merge(
-    local.existing_public_repositories,
-    local.new_public_repositories,
-  )
 }
