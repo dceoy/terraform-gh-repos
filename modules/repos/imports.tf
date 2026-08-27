5d3302db166c6066f75bec4479f584827f3a71fe
Daichi Narushima <1938249+dceoy@users.noreply.github.com>
2026-08-27 23:06:33 +0900

    Reimport preserved repository rulesets

--- modules/repos/imports.tf
+++ modules/repos/imports.tf
@@ -9,3 +9,12 @@ import {
   to       = github_workflow_repository_permissions.actions[each.key]
   id       = local.repository_names[each.key]
 }
+
+import {
+  for_each = {
+    for key, repo in local.public_repositories : key => repo
+    if repo.ruleset_id != null
+  }
+  to = github_repository_ruleset.branch[each.key]
+  id = "${local.repository_names[each.key]}:${each.value.ruleset_id}"
+}
