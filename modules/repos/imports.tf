fcae299900e76317a7e780fb9443f7caee286ec8
Daichi Narushima <1938249+dceoy@users.noreply.github.com>
2026-08-27 22:19:35 +0900

    Exclude retired repositories from imports

--- modules/repos/imports.tf
+++ modules/repos/imports.tf
@@ -1,11 +1,11 @@
 import {
-  for_each = var.repositories
+  for_each = local.active_repositories
   to       = github_repository.repo[each.key]
   id       = local.repository_names[each.key]
 }
 
 import {
-  for_each = var.repositories
+  for_each = local.active_repositories
   to       = github_workflow_repository_permissions.actions[each.key]
   id       = local.repository_names[each.key]
 }
