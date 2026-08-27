570422de61eb1d014f1e613340cee2b11ce8c303
Daichi Narushima <1938249+dceoy@users.noreply.github.com>
2026-08-27 19:57:58 +0900

    Import repositories by resolved name

--- modules/repos/imports.tf
+++ modules/repos/imports.tf
@@ -1,11 +1,11 @@
 import {
   for_each = var.repositories
   to       = github_repository.repo[each.key]
-  id       = each.key
+  id       = local.repository_names[each.key]
 }
 
 import {
   for_each = var.repositories
   to       = github_workflow_repository_permissions.actions[each.key]
-  id       = each.key
+  id       = local.repository_names[each.key]
 }
