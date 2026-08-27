3eb11aae8be176105b9c5714a3c94c8db1e94659
Daichi Narushima <1938249+dceoy@users.noreply.github.com>
2026-08-27 22:19:22 +0900

    Filter retired repositories from management

--- modules/repos/locals.tf
+++ modules/repos/locals.tf
@@ -1,9 +1,13 @@
 locals {
+  active_repositories = {
+    for key, repo in var.repositories : key => repo
+    if !repo.retired
+  }
   repository_names = {
-    for key, repo in var.repositories : key => coalesce(repo.name, key)
+    for key, repo in local.active_repositories : key => coalesce(repo.name, key)
   }
   public_repositories = {
-    for key, existing in data.github_repository.existing : key => var.repositories[key]
+    for key, existing in data.github_repository.existing : key => local.active_repositories[key]
     if existing.visibility == "public"
   }
 }
