638a561fe1574235e4ffd25dc553f7bda0b8e0f9
Daichi Narushima <1938249+dceoy@users.noreply.github.com>
2026-08-30 15:12:33 +0900

    Remove organization member management

--- modules/org/members.tf
+++
@@ -1,7 +0,0 @@
-resource "github_membership" "member" {
-  for_each = local.members_by_lower
-
-  username             = each.value.username
-  role                 = each.value.role
-  downgrade_on_destroy = each.value.downgrade_on_destroy
-}
