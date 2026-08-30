acab1abc500156bea343d8fc30d55ca83f17d10c
Daichi Narushima <1938249+dceoy@users.noreply.github.com>
2026-08-30 15:28:33 +0900

    Normalize org locals formatting

--- modules/org/locals.tf
+++ modules/org/locals.tf
@@ -4,7 +4,6 @@ locals {
       for repository in var.actions.selected_repositories : lower(repository) => repository...
     } : repository_key => sort(repositories)[0]
   }
-
   repository_metadata = {
     for repository_key, repository in data.github_rest_api.managed :
     repository_key => jsondecode(repository.body)
